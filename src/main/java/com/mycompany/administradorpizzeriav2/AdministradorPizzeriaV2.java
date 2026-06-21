145e06cb5b2353826d4de5684abc6ed2071a3ad9
AlejandroRuizH <113576772+AlejandroRuizH@users.noreply.github.com>
2026-06-21 00:51:48 -0700

    initial commit

---
+++ src/main/java/com/mycompany/administradorpizzeriav2/AdministradorPizzeriaV2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.administradorpizzeriav2;
+
+import vista.MenuPrincipal;
+
+/**
+ *
+ * @author davidalejandroruizhernandez
+ */
+public class AdministradorPizzeriaV2 {
+
+    public static void main(String[] args) {
+
+        java.awt.EventQueue.invokeLater(() -> {
+
+            new MenuPrincipal().setVisible(true);
+
+        });
+
+    }
+}
